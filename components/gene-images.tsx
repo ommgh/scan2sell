c00b3cf4b3a7cb559572c9eaeb5b174ebb0dec93
Om Mishra <om.works01@gmail.com>
2024-04-11 17:51:23 +0530

    uploadthings-conf

--- components/gene-images.tsx
+++ components/gene-images.tsx
@@ -1,41 +1,14 @@
 "use client";
 
 import { useRef, useState } from "react";
-import { ID, Client, Storage } from "appwrite";;
+import { ID, Client, Storage } from "appwrite";import { UploadDropzone } from "@/utils/uploadthing";
+;
 
 // Accessing environment variable on the client-side
 const storageId = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_ID;
 const projectId = process.env.NEXT_PUBLIC_PROJECT_STORAGE_ID;
 
-const saveImageToAppwrite = async (image: File) => {
-    const client = new Client();
-    client.setEndpoint('https://cloud.appwrite.io/v1');
-    client.setProject("#######ProjectID#########");
 
-    const storage = new Storage(client);
-    try {
-        const promise = await storage.createFile(
-            "#########StorageID############",
-            ID.unique(),
-            image // Pass the file data as an object
-        );
-         // Success
-         if (promise){
-             const fileUrl = storage.getFilePreview(
-                 "65c752846c06609469f5",
-                 promise.$id,
-              )
-             console.log(fileUrl)
-         }
-         const generate = fetch('https://image-recognition-2ginpapv2q-ue.a.run.app',{method:'POST'})
-          .then((response)=> response.json())
-          .catch((err)=>{console.log(err)})
-         
-          console.log(generate)
-    } catch (error) {
-        console.log(error)
-    }
-};
 
 
 export const GenerateImages = () => {
@@ -68,7 +41,7 @@ export const GenerateImages = () => {
                             value={"barcode"}
                             className="hidden peer"
                             required
-                            defaultChecked
+                            
                         />
                         <div className="bg-[#18676d]/20 px-6 py-2 text-lg font-semibold rounded-lg transition-colors hover:bg-[#18676d] hover:text-white peer-checked:bg-[#18676d] peer-checked:text-white">
                             Barcode
@@ -76,37 +49,68 @@ export const GenerateImages = () => {
                     </label>
 
                     <span>or</span>
+
                     <label>
                         <input
                             type="radio"
                             name="typeofimage"
-                            value={"barcode"}
+                            value={"product"}
                             className="hidden peer"
                             required
+                            defaultChecked
                         />
                         <div className="bg-[#18676d]/20 px-6 py-2 text-lg font-semibold rounded-lg transition-colors hover:bg-[#18676d] hover:text-white peer-checked:bg-[#18676d] peer-checked:text-white">
                             Product
                         </div>
                     </label>
+
+                    <span>or</span>
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="typeofimage"
+                            value={"customproduct"}
+                            className="hidden peer"
+                            required
+                            
+                        />
+                        <div className="bg-[#18676d]/20 px-6 py-2 text-lg font-semibold rounded-lg transition-colors hover:bg-[#18676d] hover:text-white peer-checked:bg-[#18676d] peer-checked:text-white">
+                            Custom
+                        </div>
+                    </label>
                 </div>
             </div>
+            {/* only for PE-II review */}
+            <div className="flex gap-[10px]">
+                <label className="text-lg font-semibold select-none">Name Product </label>
+                <input type="text" className="p-2 rounded-md bg-[#18676d]/20" />
+            </div>
+            {/* end */}
 
-            <label className="flex flex-col items-center justify-center w-3/4 h-3/4 bg-gray-500/40 outline-dashed outline-2 outline-offset-2 rounded-xl p-4 my-4">
-                <input
-                    ref={inputRef}
-                    type="file"
-                    accept="image/png, image/jpeg, image/jpeg, image/webp"
-                    className="peer hidden"
-                    onChange={updateFiles}
-                />
-                <div className="text-2xl font-semibold text-center">
-                    Upload your image
-                </div>
-                {<span>{file?.name}</span>}
-            </label>
+            <UploadDropzone
+            appearance={{
+                button:
+                  "ut-ready:bg-white ut-uploading:cursor-not-allowed rounded-r-none bg-white text-black bg-none after:bg-grey",
+                container: "w-max flex-row rounded-md border-black bg-teal-800",
+                allowedContent:
+                  "flex flex-col items-center justify-center px-2 text-teal-800",
+                label: "text-white",
+              }}
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
 
             <span
-                onClick={() => file != undefined && saveImageToAppwrite(file)}
+                // onClick={() => file != undefined && saveImageToAppwrite(file)}
                 className="bg-[#18676d]/20 px-8 py-3 text-xl font-semibold rounded-lg transition-colors hover:bg-[#18676d] hover:text-white"
             >
                 Generate Images
